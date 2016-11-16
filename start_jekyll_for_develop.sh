32def0ed528fd36e7c1135cfc697059e886e4780
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-11-16 15:19:58 +0900

    バグフィクス。Jekyll実行コマンドが間違っていたので、修正。

--- start_jekyll_for_develop.sh
+++ start_jekyll_for_develop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # デーモンモード、常時監視、外からアクセスOK、でJekyllを立ち上げる。
-jekyll serve --watch --detach --host 0.0.0.0
+jekyll serve --incremental --host 0.0.0.0
+# デーモンモードと常時監視は併用できないので、サンプルだけ置いておく。
+# jekyll serve --detach --host 0.0.0.0
